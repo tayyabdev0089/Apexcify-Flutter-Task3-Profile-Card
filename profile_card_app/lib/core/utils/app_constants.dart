a267172f5652a42a336954b72fd48a5e0da51622
Muhammad Tayyab <tayyab.dev0089@gmail.com>
2025-08-27 10:43:53 +0000

    improve some functionality and fixed bug

--- profile_card_app/lib/core/utils/app_constants.dart
+++ profile_card_app/lib/core/utils/app_constants.dart
@@ -1,12 +1,12 @@
 class AppConstants {
   static const String defaultProfileImagePath = 'assets/profile.png';
-  static const String defaultFacebookUrl = 'https://www.facebook.com/johndoe';
-  static const String defaultInstagramUrl = 'https://www.instagram.com/johndoe';
-  static const String defaultLinkedinUrl = 'https://www.linkedin.com/in/johndoe';
-  static const String defaultGithubUrl = 'https://github.com/johndoe';
-  static const String defaultName = 'John Doe';
+  static const String defaultFacebookUrl = 'https://www.facebook.com/tayyab';
+  static const String defaultInstagramUrl = 'https://www.instagram.com/tayyab';
+  static const String defaultLinkedinUrl = 'https://www.linkedin.com/in/tayyab';
+  static const String defaultGithubUrl = 'https://github.com/tayyab';
+  static const String defaultName = 'Tayyab Hassan';
   static const String defaultProfession = 'Flutter Developer';
   static const String defaultDescription = 'Passionate about building beautiful and functional mobile applications.';
-  static const String defaultPhone = '+1 234 567 890';
-  static const String defaultEmail = 'john.doe@example.com';
+  static const String defaultPhone = '+923110192166';
+  static const String defaultEmail = 'tayyab.dev0089@gmail.com';
 }
